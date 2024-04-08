5f43647133b3c0d3dc563ad484beaff70a4a2746
Adwait Barkale <adwaitbarkale@gmail.com>
2024-04-08 07:34:20 +0530

    Initial Project Setup with Resources

---
+++ Fruits App/Fruits App/Fruits_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_AppApp.swift
+//  Fruits App
+//
+//  Created by Adwait Barkale on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
